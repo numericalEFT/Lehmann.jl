baf674dcfa03563569b4d22f8dd084cc3e7474f5
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-08-03 23:08:26 -0400

    tau works

--- src/exact.jl
+++ src/exact.jl
@@ -26,21 +26,21 @@ mutable struct Basis
     end
 end
 
-function addBasis!(basis, g0::Float)
+function addBasis!(basis, proj, g0::Float)
     basis.N += 1
     if basis.N == 1
         idx = 1
         basis.grid = [g0, ]
         basis.Q = zeros(Float, (basis.N, basis.N))
-        basis.Q[1,1] = 1 / Norm(g0)
-        basis.proj = projKernel(basis)
+        basis.Q[1,1] = 1 / sqrt(proj(basis.Λ, g0, g0))
+        basis.proj = projKernel(basis, proj)
     else
         idxList = findall(x -> x > g0, basis.grid)
         # if ω is larger than any existing freqencies, then idx is an empty list
         idx = length(idxList) == 0 ? basis.N : idxList[1] # the index to insert the new frequency
 
         insert!(basis.grid, idx, g0)
-        basis.proj = projKernel(basis)
+        basis.proj = projKernel(basis, proj)
         _Q = copy(basis.Q)
         basis.Q = zeros(Float, (basis.N, basis.N))
         basis.Q[1:idx - 1, 1:idx - 1] = _Q[1:idx - 1, 1:idx - 1]
@@ -51,13 +51,13 @@ function addBasis!(basis, g0::Float)
         basis.Q[idx, :] = mGramSchmidt(basis, idx, g0)
     end
 
-    candidate, residual = scanResidual!(basis, g0, idx)
+    candidate, residual = scanResidual!(basis, proj, g0, idx)
     insert!(basis.residual, idx, maximum(residual))
 # scanResidual!(basis, idx-9, idx+10, g0)
     return idx, candidate, residual
 end
 
-function scanResidual!(basis, g0, idx)
+function scanResidual!(basis, proj, g0, idx)
     candidate = zeros(Float, basis.N)
     residual = zeros(Float, basis.N)
     for i in 1:basis.N # because of the separation of scales, the grids far away from idx is rarely affected
@@ -67,35 +67,35 @@ function scanResidual!(basis, g0, idx)
         elseif i == basis.N
             # take care of the last bin
             if g0 < eps(Float(0))
-                g = findCandidate(basis, Float(0), Float(10))
+                g = findCandidate(basis, proj, Float(0), Float(10))
             else
-                g = findCandidate(basis, basis.grid[end], basis.grid[end] * 10)
+                g = findCandidate(basis, proj, basis.grid[end], basis.grid[end] * 10)
             end
             if g > basis.Λ
                 candidate[end] = basis.Λ
-                residual[end] = Residual(basis, basis.Λ)
+                residual[end] = Residual(basis, proj, basis.Λ)
             else
                 candidate[end] = g
-                residual[end] = Residual(basis, g)
+                residual[end] = Residual(basis, proj, g)
             end
         else # 1<=i<=basis.N-1
-            g = findCandidate(basis, basis.grid[i], basis.grid[i + 1])
+            g = findCandidate(basis, proj, basis.grid[i], basis.grid[i + 1])
             candidate[i] = g
-            residual[i] = Residual(basis, g)
+            residual[i] = Residual(basis, proj, g)
         end
     end
     return candidate, residual
 end
 
-function QR(Λ, rtol)
+function QR(Λ, rtol, proj, isMatfreq; N=nothing)
     basis = Basis(Λ, rtol)
-    idx, candidate, residual = addBasis!(basis, Float(0))
+    idx, candidate, residual = addBasis!(basis, proj, Float(0))
     maxResidual, ωi = findmax(residual)
 
-    while maxResidual > rtol
+    while isnothing(N) ? maxResidual > rtol : basis.N < N
 
         newω = candidate[ωi]
-        idx, candidate, residual = addBasis!(basis, newω)
+        idx, candidate, residual = addBasis!(basis, proj, newω)
 
         @printf("%3i : ω=%24.8f ∈ (%24.8f, %24.8f) -> error=%24.16g\n", basis.N, newω, basis.grid[idx - 1], (idx == basis.N) ? Λ : basis.grid[idx + 1], basis.residual[idx])
         # println("$(length(freq)) basis: ω=$(Float64(newω)) between ($(Float64(freq[idx - 1])), $(Float64(freq[idx + 1])))")
@@ -117,7 +117,7 @@ function QR(Λ, rtol)
     @printf("residual = %.10e\n", maxResidual)
 
     ω = LinRange(Float(0), Float(100), 1000)
-    y = [Residual(basis, w) for w in ω]
+    y = [Residual(basis, proj, w) for w in ω]
     p = plot(ω, y, xlims=(0.0, 100))
     plot!(p, candidate, residual, seriestype=:scatter)
 display(p)
@@ -126,24 +126,22 @@ display(p)
     return basis
 end
 
-Norm(ω) = sqrt(abs(proj(ω, ω)))
-
-
-
 """
 q1=sum_j c_j K_j
 q2=sum_k d_k K_k
 return <q1, q2> = sum_jk c_j*d_k <K_j, K_k>
 """
-function proj(basis, q1::Vector{Float}, q2::Vector{Float})
+function projqq(basis, q1::Vector{Float}, q2::Vector{Float})
     return q1' * basis.proj * q2
     end
-    
-        function projKernel(basis)
-            K = zeros(Float, (basis.N, basis.N))
+"""
+<K(g_i), K(g_j)>
+"""    
+function projKernel(basis, proj)
+    K = zeros(Float, (basis.N, basis.N))
     for i in 1:basis.N
     for j in 1:basis.N
-            K[i,j] = proj(basis.grid[i], basis.grid[j])
+            K[i,j] = proj(basis.Λ, basis.grid[i], basis.grid[j])
         end
     end
     return K
@@ -161,12 +159,10 @@ function mGramSchmidt(basis, idx, g::Float)
             continue
         end
         q = basis.Q[qi, :]
-        # println(proj(basis.grid, q, qnew), " vs ",q'*basis.proj*qnew)
-        # qnew = qnew - proj(basis.grid, q, qnew) .* q
-    qnew -= proj(basis, q, qnew) .* q  # <q, qnew> q
+        qnew -= projqq(basis, q, qnew) .* q  # <q, qnew> q
 end
 
-    norm = sqrt(proj(basis, qnew, qnew))
+    norm = sqrt(projqq(basis, qnew, qnew))
     return qnew / norm
 end
 
@@ -180,46 +176,46 @@ function GramSchmidt(basis, idx, g::Float)
         
     for qi in 1:basis.N
         if qi == idx
-            continue
-        end
+    continue
+    end
         q = basis.Q[qi, :]
-        qnew -=  proj(basis, q, q0) .* q
+        qnew -=  projqq(basis, q, q0) .* q
     end
     
-    norm = sqrt(proj(basis, qnew, qnew))
+    norm = sqrt(projqq(basis, qnew, qnew))
     return qnew / norm
 end
 
-function Residual(basis, g::Float)
+function Residual(basis, proj, g::Float)
     # norm2 = proj(g, g) - \sum_i (<qi, K_g>)^2
     # qi=\sum_j Q_ij K_j ==> (<qi, K_g>)^2 = (\sum_j Q_ij <K_j, K_g>)^2 = \sum_jk Q_ij*Q_ik <K_j, K_g>*<K_k, Kg>
     
-    KK = [proj(gj, g) for gj in basis.grid]
-    norm2 = proj(g, g) - (norm(basis.Q * KK))^2
+    KK = [proj(basis.Λ, gj, g) for gj in basis.grid]
+    norm2 = proj(basis.Λ, g, g) - (norm(basis.Q * KK))^2
     return norm2 < 0 ? Float(0) : sqrt(norm2) 
 end
     
-function findCandidate(basis, gmin::Float, gmax::Float)
+function findCandidate(basis, proj, gmin::Float, gmax::Float)
     N = 16
     dg = abs(gmax - gmin) / N
-    r0 = Residual(basis, gmin)
+    r0 = Residual(basis, proj, gmin)
     g = gmin + dg
-    r = Residual(basis, g)
-    if r < r0
+        r = Residual(basis, proj, g)
+        if r < r0
        println("warning: $r at $g < $r0 at $gmin  !")
 
-    #    ω = LinRange(Float(0), Float(gmax), 1000)
-    #    y = [Residual(basis, w) for w in ω]
-    #    p = plot(ω, y, xlims=(0.0, 10))
-    #    display(p)
-    #    readline()
-    
-    exit(0)
+       ω = LinRange(Float(0), Float(10), 1000)
+        y = [Residual(basis, proj, w) for w in ω]
+       p = plot(ω, y, xlims=(0.0, 10))
+       display(p)
+       readline()
+
+       exit(0)
     end
     while r > r0
     g += dg
         r0 = r
-    r = Residual(basis, g)
+    r = Residual(basis, proj, g)
     end
     return g - dg
 end
@@ -236,7 +232,10 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__    
     # freq, Q = findBasis(1.0e-3, Float(100))
     # basis = QR(100, 1e-3)
-    @time basis = QR(100000000, 1e-10)
+    Λ = 1000000
+    # Λ = 100
+    @time ωBasis = QR(Λ, 1e-10, projPH_ω, false)
+    @time τBasis = QR(Λ / 2, 1e-11, projPH_τ, false, N=ωBasis.N)
     # @time basis = QR(100, 1e-10)
     # readline()
     # basis = QR(100, 1e-3)
--- src/kernel.jl
+++ src/kernel.jl
@@ -1,5 +1,5 @@
 """
-\\int_0^1 e^{-ω_1 τ}*e^{-ω_2*τ} dτ = (1-exp(-(ω_1+ω_2))/(ω_1+ω_2)
+(1-exp(-Λ*x)/x
 """
 function kernel(ω::Float)
     if ω < 1e-5
@@ -10,13 +10,50 @@ function kernel(ω::Float)
     end
 end
 
+##################### Particle-hole symmetric kernel #############################
+
 """
-(1-e^{ω1+ω2})/(ω1+ω2)+(e^{-ω2}-e^{-ω1})/(ω1-ω2)
+particle-hole symmetric kernel: K(ω, τ)=e^{-ω*τ}+e^{-ω*(β-τ)}
+
+KK=int_0^{1/2} dτ K(ω1,τ)*K(ω2,τ)=(1-e^{ω1+ω2})/(ω1+ω2)+(e^{-ω2}-e^{-ω1})/(ω1-ω2)
 """
-function proj(ω1::Float, ω2::Float)
-    if ω1 > ω2
+function projPH_ω(Λ::Float, ω1::Float, ω2::Float)
+        if ω1 > ω2
         return kernel(ω1 + ω2) + exp(-ω2) * kernel(ω1 - ω2)
     else
         return kernel(ω1 + ω2) + exp(-ω1) * kernel(ω2 - ω1)
     end
+end
+
+"""
+particle-hole symmetric kernel: K(ω, τ)=e^{-ω*τ}+e^{-ω*(β-τ)}
+
+KK=int_0^{Λ} dτ K(ω,t1)*K(ω2,t2)=(1-e^{t1+t2})/(t1+t2)+(1-e^{2β-t1-t2})/(2β-t1-t2)+(1-e^{β+t1-t2})/(β+t1-t2)+(1-e^{β-t1+t2})/(β-t1+t2)
+"""
+function projPH_τ(Λ::Float, t1::Float, t2::Float)
+    # note that here Λ = \beta/2
+    return kernel(t1 + t2) + kernel(4 * Λ - t1 - t2) + kernel(2 * Λ - t1 + t2) + kernel(2 * Λ + t1 - t2)
+end
+
+##################### Particle-hole asymmetric kernel #############################
+"""
+particle=hole asymmetric kernel: K(ω, τ)=e^{-ω*τ}-e^{-ω*(β-τ)}
+
+KK=int_0^{1/2} dτ K(ω1,τ)*K(ω2,τ)=(1-e^{ω1+ω2})/(ω1+ω2)-(e^{-ω2}-e^{-ω1})/(ω1-ω2)
+"""
+function projPHA_ω(Λ::Float, ω1::Float, ω2::Float)
+    if ω1 > ω2
+    return kernel(ω1 + ω2) - exp(-ω2) * kernel(ω1 - ω2)
+    else
+    return kernel(ω1 + ω2) - exp(-ω1) * kernel(ω2 - ω1)
+    end
+end
+
+"""
+particle-hole asymmetric kernel: K(ω, τ)=e^{-ω*τ}-e^{-ω*(β-τ)}
+
+KK=int_0^{Λ} dτ K(ω,t1)*K(ω2,t2)=(1-e^{t1+t2})/(t1+t2)+(1-e^{2β-t1-t2})/(2β-t1-t2)-(1-e^{β+t1-t2})/(β+t1-t2)-(1-e^{β-t1+t2})/(β-t1+t2)
+"""
+function projPHA_τ(Λ::Float, t1::Float, t2::Float)
+    return kernel(t1 + t2) + kernel(4 * Λ - t1 - t2) - kernel(2 * Λ - t1 + t2) - kernel(2 * Λ + t1 - t2)
 end
