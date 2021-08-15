482b195114e5c5f3aab49699f45c4ea192d2393c
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-08-14 22:38:52 -0400

    wip

--- src/vertex3/builder.jl
+++ src/vertex3/builder.jl
@@ -23,12 +23,32 @@ include("./kernel.jl")
 #     readline()
 # end
 
-using GR
+# using GR
+# function plotResidual(basis)
+#     z = Float64.(basis.residualFineGrid)
+#     z = reshape(z, (basis.Nfine, basis.Nfine))
+#     # contourf(z)
+#     p = heatmap(basis.fineGrid, basis.fineGrid, z)
+#     # display(p)
+#     readline()
+# end
+# gr()
+# data = rand(21, 100)
+# heatmap(1:size(data, 1),
+#     1:size(data, 2), data,
+#     c=cgrad([:blue, :white,:red, :yellow]),
+#     xlabel="x values", ylabel="y values",
+#     title="My title")
+
+using Plots
+gr()
+
 function plotResidual(basis)
     z = Float64.(basis.residualFineGrid)
     z = reshape(z, (basis.Nfine, basis.Nfine))
     # contourf(z)
-    p = heatmap(z)
+    p = heatmap(basis.fineGrid, basis.fineGrid, z)
+    # p = heatmap(z)
     display(p)
     readline()
 end
@@ -56,13 +76,22 @@ mutable struct Basis
         _grid = Matrix{Float}(undef, (0, 0))
         _finegrid = unilog(Λ, rtol)
         Nfine = length(_finegrid)
-        _residualFineGrid = zeros(Nfine^d)
+        _residualFineGrid = zeros(Float, Nfine^d)
 
         # initialize the residual on fineGrid with <g, g>
         for (gi, g) in enumerate(iterateFineGrid(d, _finegrid))
             _residualFineGrid[gi] = projector(Λ, dim, g, g)
         end
-        println(_residualFineGrid)
+
+        # g1 = (_finegrid[1], _finegrid[1])
+        # println("compare: ", abs(projector(basis.Λ, basis.D, g1, g1) - _residualFineGrid[1]))
+        # for idx in 1:Nfine^d
+        #     c = idx2coord(d, Nfine, idx)
+        #     # println(c)
+        #     g = (_finegrid[c[1]], _finegrid[c[2]])
+        #     _residualFineGrid[idx] = projector(Λ, d, g, g)
+        # end
+        # println(_residualFineGrid)
 
         return new(d, Λ, rtol, 0, _grid, [], _Q, similar(_Q), Nfine, _finegrid, _residualFineGrid)
     end
@@ -78,12 +107,24 @@ function iterateFineGrid(dim, _finegrid)
     end
 end
 
+function idx2coord(dim, N, idx)
+    return ((idx - 1) ÷ N + 1, (idx - 1) % N + 1)
+end
+
+function coord2idx(dim, N, coord)
+    return (coord[1] - 1) * N + coord[2]
+end
+
 """
 composite expoential grid
 """
 function unilog(Λ, rtol)
-    degree = 2
-    ratio = Float(1.5)
+    # N = 500
+    # grid = [i / N * Λ for i in 1:N]
+    # return grid
+
+    degree = 8
+    ratio = Float(1.2)
     N = Int(floor(log(Λ) / log(ratio) + 1))
     panel = [Λ / ratio^(N - i) for i in 1:N]
     grid = Vector{Float}(undef, 0)
@@ -111,28 +152,35 @@ function addBasis!(basis, projector, g0)
         basis.Q[1,1] = 1 / sqrt(projector(basis.Λ, basis.D, g0, g0))
     else
         basis.grid[1:end - 1, :] = _grid
-        basis.grid[end, :] = g0
+        # println(g0)
+        basis.grid[end, :] .= g0
+        # println(basis.grid[end, :])
         basis.proj = projKernel(basis, projector)
         basis.Q[1:end - 1, 1:end - 1] = _Q
         basis.Q[end, :] = mGramSchmidt(basis, g0)
     end
 
+    # println(basis.N)
     updateResidual!(basis, projector)
-    append!(basis.residual, maximum(basis.residualFineGrid)) # record error after the new grid is added
+    append!(basis.residual, sqrt(maximum(basis.residualFineGrid))) # record error after the new grid is added
 
 end
 
 function updateResidual!(basis, projector)
     q = basis.Q[end, :]
-    for (idx, g) in enumerate(iterateFineGrid(basis.D, basis.fineGrid))
+    # println(q)
+    # for (idx, g) in enumerate(iterateFineGrid(basis.D, basis.fineGrid))
+    for idx in 1:basis.Nfine^basis.D
+        c = idx2coord(basis.D, basis.Nfine, idx)
+        # println(c)
+        g = (basis.fineGrid[c[1]], basis.fineGrid[c[2]])
         proj = Float(0)
         for j in 1:basis.N
             proj += q[j] * projector(basis.Λ, basis.D, g, basis.grid[j, :])
         end
-        # return norm2 < 0 ? Float(0) : sqrt(norm2) 
+
         basis.residualFineGrid[idx] -= proj^2
     end
-    # println(basis.residualFineGrid)
 end
 
 function QR(dim, Λ, rtol, proj, g0; N=nothing)
@@ -140,16 +188,26 @@ function QR(dim, Λ, rtol, proj, g0; N=nothing)
     for g in g0
         addBasis!(basis, proj, g)
         @printf("%3i : ω=(%24.8f, %24.8f) -> error=%24.16g\n", 1, g[1], g[2], basis.residual[end])
-        plotResidual(basis)
     end
-    maxResidual, ωi = findmax(residual)
+    maxResidual, idx = findmax(basis.residualFineGrid)
 
     while isnothing(N) ? maxResidual > rtol / 10 : basis.N < N
 
-        g = candidate[ωi]
+        c = idx2coord(basis.D, basis.Nfine, idx)
+        g = (basis.fineGrid[c[1]], basis.fineGrid[c[2]])
+        residual = Residual(basis, proj, g)
+
         addBasis!(basis, proj, g)
         @printf("%3i : ω=(%24.8f, %24.8f) -> error=%24.16g\n", basis.N, g[1], g[2], basis.residual[end])
-        maxResidual, ωi = findmax(residual)
+
+        if abs(g[1] - g[2]) > 1e-8
+            gp = (g[2], g[1])
+            addBasis!(basis, proj, gp)
+            @printf("%3i : ω=(%24.8f, %24.8f) -> error=%24.16g\n", basis.N, gp[1], gp[2], basis.residual[end])
+        end
+
+        maxResidual, idx = findmax(basis.residualFineGrid)
+
         plotResidual(basis)
     end
     testOrthgonal(basis)
@@ -184,11 +242,15 @@ modified Gram-Schmidt process
     function mGramSchmidt(basis, g)
     qnew = zeros(Float, basis.N)
     qnew[end] = 1
+    # println(basis.proj)
         
     for qi in 1:basis.N - 1
         q = basis.Q[qi, :]
         qnew -= projqq(basis, q, qnew) .* q  # <q, qnew> q
+        # println("q: ", q)
+        # println("proj:", projqq(basis, q, qnew))
     end
+    # println(qnew)
     return qnew / sqrt(projqq(basis, qnew, qnew))
 end
 
@@ -212,14 +274,19 @@ end
 #     return qnew / norm
 # end
 
-# function Residual(basis, proj, g::Float)
-#     # norm2 = proj(g, g) - \sum_i (<qi, K_g>)^2
-#     # qi=\sum_j Q_ij K_j ==> (<qi, K_g>)^2 = (\sum_j Q_ij <K_j, K_g>)^2 = \sum_jk Q_ij*Q_ik <K_j, K_g>*<K_k, Kg>
+function Residual(basis, proj, g)
+    # norm2 = proj(g, g) - \sum_i (<qi, K_g>)^2
+    # qi=\sum_j Q_ij K_j ==> (<qi, K_g>)^2 = (\sum_j Q_ij <K_j, K_g>)^2 = \sum_jk Q_ij*Q_ik <K_j, K_g>*<K_k, Kg>
     
-#     KK = [proj(basis.Λ, gj, g) for gj in basis.grid]
-#     norm2 = proj(basis.Λ, g, g) - (norm(basis.Q * KK))^2
-#     return norm2 < 0 ? Float(0) : sqrt(norm2) 
-#     end
+    KK = [proj(basis.Λ, basis.D, basis.grid[j, :], g) for j in 1:basis.N]
+    norm2 = proj(basis.Λ, basis.D, g, g) - (norm(basis.Q * KK))^2
+    # norm2 = proj(basis.Λ, basis.D, g, g)
+    # for i in 1:basis.N
+    #     norm2 -= (sum(basis.Q[i, :] .* KK))^2
+    # end
+    return norm2 < 0 ? Float(0) : sqrt(norm2) 
+    # return norm2
+end
         
 function testOrthgonal(basis)
     println("testing orthognalization...")
@@ -243,7 +310,7 @@ function dlr_functional(type, Λ, rtol)
     # # τBasis = QR(Λ / 2, rtol / 10, projPH_τ, Float(0), N=ωBasis.N)
     # println("Building n grid ... ")
     # nBasis = MatFreqGrid(ωBasis.grid, ωBasis.N, Λ, :corr)
-
+    
     rank = ωBasis.N
     ωGrid = ωBasis.grid
     # τGrid = τBasis / Λ
@@ -261,10 +328,11 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__    
     # freq, Q = findBasis(1.0e-3, Float(100))
     # basis = QR(100, 1e-3)
-    Λ = Float(1e3)
+    Λ = Float(100)
     rtol = Float(1e-8)
     dim = 2
-    g0 = [[Float(0), Float(0)], [Float(0), Λ], [Λ, Float(0)], [Λ, Λ]]
+    # g0 = [[Float(0), Float(0)], [Float(0), Λ], [Λ, Float(0)], [Λ, Λ]]
+    g0 = [[Λ, Λ], [Float(0), Λ], [Λ, Float(0)]]
     # println(unilog(Λ, rtol))
     # Λ = 100
     @time ωBasis = QR(dim, Λ, rtol, projExp_τ, g0)
