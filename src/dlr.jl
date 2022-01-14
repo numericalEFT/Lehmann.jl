e4081252e70411739b4cdea0b0410f22ca8d146d
Kun Chen <chenkun0228@gmail.com>
2022-01-14 15:50:14 -0500

    allow beta as the input

--- src/dlr.jl
+++ src/dlr.jl
@@ -47,20 +47,21 @@ mutable struct DLRGrid
     """
     function DLRGrid(Euv, β, rtol, isFermi::Bool; symmetry::Symbol = :none, rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
     function DLRGrid(; Euv, β, isFermi::Bool, symmetry::Symbol = :none, rtol = 1e-8, rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
+    function DLRGrid(; Euv, beta, isFermi::Bool, symmetry::Symbol = :none, rtol = 1e-8, rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
 
     Create DLR grids
 
     #Arguments:
-    - `Euv`       : the UV energy scale of the spectral density 
-    - `β`         : inverse temeprature
-    - `isFermi`   : bool is fermionic or bosonic
-    - `symmetry`  : particle-hole symmetric :ph, or particle-hole asymmetric :pha, or :none
-    - `rtol`      : tolerance absolute error
-    - `rebuild`   : set false to load DLR basis from the file, set true to recalculate the DLR basis on the fly
-    - `folder`    : if rebuild is true and folder is set, then dlrGrid will be rebuilt and saved to the specified folder
-                    if rebuild is false and folder is set, then dlrGrid will be loaded from the specified folder
-    - `algorithm` : if rebuild = true, then set :functional to use the functional algorithm to generate the DLR basis, or set :discrete to use the matrix algorithm.
-    - `verbose`   : false not to print DLRGrid to terminal, or true to print
+    - `Euv`         : the UV energy scale of the spectral density 
+    - `β` or `beta` : inverse temeprature
+    - `isFermi`     : bool is fermionic or bosonic
+    - `symmetry`    : particle-hole symmetric :ph, or particle-hole asymmetric :pha, or :none
+    - `rtol`        : tolerance absolute error
+    - `rebuild`     : set false to load DLR basis from the file, set true to recalculate the DLR basis on the fly
+    - `folder`      : if rebuild is true and folder is set, then dlrGrid will be rebuilt and saved to the specified folder
+                      if rebuild is false and folder is set, then dlrGrid will be loaded from the specified folder
+    - `algorithm`   : if rebuild = true, then set :functional to use the functional algorithm to generate the DLR basis, or set :discrete to use the matrix algorithm.
+    - `verbose`     : false not to print DLRGrid to terminal, or true to print
     """
     function DLRGrid(Euv, β, rtol, isFermi::Bool, symmetry::Symbol = :none; rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
         Λ = Euv * β # dlr only depends on this dimensionless scale
@@ -146,6 +147,9 @@ mutable struct DLRGrid
 
     function DLRGrid(; Euv, β, isFermi::Bool, symmetry::Symbol = :none, rtol = 1e-14, rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
         return DLRGrid(Euv, β, rtol, isFermi, symmetry; rebuild = rebuild, folder = folder, algorithm = algorithm, verbose = verbose)
+    end
+    function DLRGrid(; Euv, beta, isFermi::Bool, symmetry::Symbol = :none, rtol = 1e-14, rebuild = false, folder = nothing, algorithm = :functional, verbose = false)
+        return DLRGrid(Euv, beta, rtol, isFermi, symmetry; rebuild = rebuild, folder = folder, algorithm = algorithm, verbose = verbose)
     end
 end
 
