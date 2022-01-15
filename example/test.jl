e8945c7600ebcf4291130fb13036bf75bdd5dd01
Kun Chen <chenkun0228@gmail.com>
2022-01-14 22:55:21 -0500

    bad again

--- example/test.jl
+++ example/test.jl
@@ -1,10 +1,10 @@
 using Lehmann
 using Gaston
-dlr = DLRGrid(β = 1000.0, isFermi = true, rtol = 1e-6, symmetry = :ph)
-G = Sample.SemiCircle(dlr, :τ)
-coeff2 = tau2dlr(dlr, G, sumrule = π / 2)
-println(coeff2)
-println("diff: ", sum(coeff2), ", ", sum(coeff2) - π / 2)
+# dlr = DLRGrid(β = 1000.0, isFermi = true, rtol = 1e-6, symmetry = :ph)
+# G = Sample.SemiCircle(dlr, :τ)
+# coeff2 = tau2dlr(dlr, G, sumrule = π / 2)
+# println(coeff2)
+# println("diff: ", sum(coeff2), ", ", sum(coeff2) - π / 2)
 
 dlr = DLRGrid(β = 1000.0, isFermi = true, rtol = 1e-6, symmetry = :none)
 G = Sample.SemiCircle(dlr, :τ)
