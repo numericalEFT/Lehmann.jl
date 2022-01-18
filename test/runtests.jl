1ba1bd0325ee33d84fdf4fb6c6337292d0cc175a
Kun Chen <chenkun0228@gmail.com>
2022-01-18 16:50:40 -0500

    all test passed

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,8 @@ using Base: BufferStream
 using Lehmann
 using Test
 using Printf
-using JLD2, CodecZlib
+using JLD2
+using CodecZlib #it is important to import CodecZlib explicitly. Otherwise, the code may try to dynamically load this package, and sometimes leads to error
 
 include("spectral.jl")
 include("dlr.jl")
